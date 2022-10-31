80b6e496c01956a6a66eb90d3ec4a6d9375976a1
j1nxie <rylieeeeexd@gmail.com>
2022-10-31 21:01:04 +0700

    [solution] day 01 + 02

---
+++ src/bin/01.rs
@@ -0,0 +1,52 @@
+pub fn part_one(input: &str) -> Option<i32> {
+    let mut floors = 0;
+    for c in input.chars() {
+        if String::from(c) == "(" {
+            floors += 1;
+        } else if String::from(c) == ")" {
+            floors -= 1;
+        }
+    }
+    return Some(floors);
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    let mut count = 0;
+    let mut floors: i64 = 0;
+    for c in input.chars() {
+        if String::from(c) == "(" {
+            floors += 1;
+            count += 1;
+        } else if String::from(c) == ")" {
+            floors -= 1;
+            count += 1;
+        }
+        if floors == -1 {
+            return Some(count);
+        }
+    }
+    return Some(0);
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 1);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(0));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 1);
+        assert_eq!(part_two(&input), Some(0));
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,86 @@
+use std::cmp;
+
+pub type Gift = (u32, u32, u32);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let gifts: Vec<Gift> = input
+        .lines()
+        .map(|i| {
+            let mut gift = i.trim().split('x').map(|d| d.parse().unwrap());
+            (
+                gift.next().unwrap(),
+                gift.next().unwrap(),
+                gift.next().unwrap(),
+            )
+        })
+        .collect();
+
+    let mut total = 0;
+
+    for gift in gifts {
+        let top = 2 * gift.0 * gift.1;
+        let side = 2 * gift.1 * gift.2;
+        let front = 2 * gift.2 * gift.0;
+
+        let cmp = cmp::min(top, side);
+        let min = cmp::min(cmp, front);
+
+        total += top + side + front + min / 2;
+    }
+
+    return Some(total);
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let gifts: Vec<Gift> = input
+        .lines()
+        .map(|i| {
+            let mut gift = i.trim().split('x').map(|d| d.parse().unwrap());
+            (
+                gift.next().unwrap(),
+                gift.next().unwrap(),
+                gift.next().unwrap(),
+            )
+        })
+        .collect();
+
+    let mut total = 0;
+
+    for gift in gifts {
+        let smallest_side: (u32, u32) = {
+            let mut vec = vec![gift.0, gift.1, gift.2];
+            vec.sort();
+
+            (vec[0], vec[1])
+        };
+
+        let volume = gift.0 * gift.1 * gift.2;
+
+        total += 2 * (smallest_side.0 + smallest_side.1) + volume;
+    }
+
+    return Some(total);
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 2);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 2);
+        assert_eq!(part_one(&input), Some(168));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 2);
+        assert_eq!(part_two(&input), Some(138));
+    }
+}
