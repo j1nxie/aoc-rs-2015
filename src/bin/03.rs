808f77ba9786f18e38d68f5cfdda815cbfe6046a
j1nxie <rylieeeeexd@gmail.com>
2022-10-31 21:27:21 +0700

    [solution] day 03

---
+++ src/bin/03.rs
@@ -0,0 +1,75 @@
+use std::collections::HashSet;
+
+pub type Position = (i32, i32);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut houses: HashSet<Position> = HashSet::new();
+    let mut current_pos = (0, 0);
+
+    houses.insert(current_pos);
+
+    for &c in input.as_bytes() {
+        match c {
+            b'>' => current_pos.0 += 1,
+            b'<' => current_pos.0 -= 1,
+            b'^' => current_pos.1 += 1,
+            b'v' => current_pos.1 -= 1,
+            _ => continue,
+        }
+
+        houses.insert(current_pos);
+    }
+
+    Some(houses.len().try_into().unwrap())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut houses: HashSet<Position> = HashSet::new();
+    let mut santa = (0, 0);
+    let mut robo_santa = (0, 0);
+
+    houses.insert(santa);
+
+    for (i, &c) in input.as_bytes().iter().enumerate() {
+        let current_pos = if i % 2 == 0 {
+            &mut santa
+        } else {
+            &mut robo_santa
+        };
+
+        match c {
+            b'>' => current_pos.0 += 1,
+            b'<' => current_pos.0 -= 1,
+            b'^' => current_pos.1 += 1,
+            b'v' => current_pos.1 -= 1,
+            _ => continue,
+        }
+
+        houses.insert(*current_pos);
+    }
+
+    Some(houses.len().try_into().unwrap())
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 3);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 3);
+        assert_eq!(part_one(&input), Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 3);
+        assert_eq!(part_two(&input), Some(3));
+    }
+}
